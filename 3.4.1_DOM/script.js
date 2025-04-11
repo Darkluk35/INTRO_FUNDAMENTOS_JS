ef4eebf7edbf9eb4543102cd949ba6ea0928d2bc
Luis <jesuslara.luis99@gmail.com>
2025-04-10 20:39:34 -0600

    Practica en clase incompleta

--- 3.4.1_DOM/script.js
+++ 3.4.1_DOM/script.js
@@ -1,3 +1,14 @@
+//DOM: Document Object Model
+/*
+    Model -> Representacion de algo.
+    DOM es la representacion de una pagina HTML para JS
+    Esta representacion es a traves de una estructura conocida como arbol.
+    El arbol se compone de eementos conocidos como nodos los cuales representan un elemento de HTML
+
+
+
+*/
+
 function agregar_tarea() {
     // Accediendo al elemento input.
     let agreagarTareaInput = document.getElementById("AgregarTareaInput");
@@ -15,6 +26,15 @@ function agregar_tarea() {
     let nuevoLi = document.createElement("li");
     nuevoLi.innerText = agreagarTareaInput.value; //El atributo value me permite acceder a lo que esta
 // escrito en el input
+
+
+let parrafoNuevo = document.createElement("p");
+parrafoNuevo.innerText = agreagarTareaInput.value;
+nuevoLi.appendChild(parrafoNuevo); //Agregando el parrafo al nuevo li
+let botonEliminar = document.createElement("button");
+
+parrafoNuevo.appendChild(botonEliminar);
+botonEliminar.innerText = "Eliminar";
 
     //Una vez creado el nuevo elemento, se debe agregar a otro elemento
     listaTareaUl.appendChild(nuevoLi)
@@ -46,4 +66,24 @@ agregarTareabtn.addEventListener("click",agregar_tarea);
 
 //NOTA: Cuando trabajamos con addEventListener, los eventos NO deben llevar la palabra "on" al inicio.
 
+//Funcion -> Un bloque de codigo reutilizable
+
+
+
+//Tambien hay funciones que no tienen nombre y se le denominan anonimas:
+//Funciones que no tienen nombre
+//En JS se les conoce como funciones flecha
+/*
+
+(param1, param2, param3,..., paramN )>= {
+    Codigo a ejecutar
+}
+*/
+
+let funcionSuma = (a, b) => {
+    return a + b;
+}
+//Ejemplo de una funcion flecha
+funcionSuma(5,1)
+//SOlo se ejecutan una vez en el codigo
 
